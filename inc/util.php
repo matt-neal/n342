3fadfd3a4acc0810b103fe0655ecfbc9dc5e4656
Matt Neal <mattneal@umail.iu.edu>
2016-10-16 20:00:40 -0400

    folder updates

--- inc/util.php
+++ inc/util.php
@@ -76,7 +76,27 @@ function sqlReplace($text)
     $text = htmlspecialchars($text, ENT_QUOTES);
         
   	return $text;
-} 
+}
 
+/*This function prevents malicious users enter multiple email addresses into the email box
+*It makes sure that only one email is entered into the email box.
+*/
+function spamcheck($field)
+{
+    //filter_var() sanitizes the e-mail
+    //address using FILTER_SANITIZE_EMAIL
+    $field=filter_var($field, FILTER_SANITIZE_EMAIL);
+
+    //filter_var() validates the e-mail
+    //address using FILTER_VALIDATE_EMAIL
+    if(filter_var($field, FILTER_VALIDATE_EMAIL))
+    {
+        return TRUE;
+    }
+    else
+    {
+        return FALSE;
+    }
+}
 
 ?>
--- lab4/lab4.php
+++ lab4/lab4.php
@@ -7,10 +7,14 @@ labThree.php
 PLEASE READ THIS FILE and LOGIN.PHP and ADMIN.PHP
 -->
 
-<?php
-if(!isset( $_SESSION)) {
-    session_start();
-}
+<?php session_start(); //this must be the very first line on the php page, to register this page to use session variables
+$_SESSION['timeout'] = time();
+
+//if this is a page that requires login always perform this session verification
+require_once "../inc/sessionVerify.php";
+
+require_once "../inc/dbconnect.php";
+
 ?>
 
 <!DOCTYPE HTML>
