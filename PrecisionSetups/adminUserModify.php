9712f52b19095b10cc46ede4397ed92047cfd4a2
Matt Neal <mattneal@umail.iu.edu>
2016-11-17 09:51:43 -0500

    Correct changeable database fields.

--- PrecisionSetups/adminUserModify.php
+++ PrecisionSetups/adminUserModify.php
@@ -21,38 +21,29 @@ include "./header.php";
 <!-- Wrapper -->
 <div id="wrapper">
 
-    <form action="registration.php" method="post">
+    <form action="adminUserModify.php" method="post">
 
         <?php print $msg; ?>
 
-        <label for="firstName">First Name: <?php print $firstNameRequired; ?></label>
-        <input type="text" id="firstName" placeholder="Bobby" name="firstName" value="<?php print $fName; ?>" required>
-
-        <label for="lastName">Last Name: <?php print $lastNameRequired; ?></label>
-        <input type="text" id="lastName" placeholder="Tables" name="lastName" value="<?php print $lName; ?>" required>
-
         <label for="email">Email: <?php print $emailRequired; ?></label>
-        <input type="email" id="email" placeholder="btables@iupui.edu" name="email" value="<?php print $eMail; ?>" required>
+        <input type="email" id="email" placeholder="btables@iupui.edu" name="email" value="<?php print $eMail; ?>">
 
         <label for="emailConfirm">Confirm Email:</label>
-        <input type="email" id="emailConfirm" name="emailConfirm" placeholder="Please Confirm Email" value="<?php print $emailConfirmation; ?>" required>
+        <input type="email" id="emailConfirm" name="emailConfirm" placeholder="Please Confirm Email" value="<?php print $emailConfirmation; ?>">
 
-        <label for="password">Password: Must contain 10-18 characters, with at least 1 letter and 1 number. <?php print $passwordRequired; ?></label>
-        <input type="password" id="password" name="password" value="<?php print $pWord; ?>" required>
+        <label for="password">Change Password: <?php print $passwordRequired; ?></label>
+        <input type="password" id="password" name="password" value="<?php print $pWord; ?>" >
 
-        <label for="password">Confirm Password:</label>
-        <input type="password" id="passwordConfirm" placeholder="Please Confirm Password" name="passwordConfirm" value="<?php print $passwordConfirmation; ?>" required>
+        <label for="password">Confirm New Password:</label>
+        <input type="password" id="passwordConfirm" placeholder="Please Confirm Password" name="passwordConfirm" value="<?php print $passwordConfirmation; ?>" >
 
-        <label for="homePhone">Home Phone: (Format: 555-555-5555)</label>
-        <input type="text" id="homePhone" placeholder="Please Enter Home Phone #" name="homePhone" value="<?php print $homePhone; ?>" required>
+        <label for="homePhone">Change Home Phone: (Format: 555-555-5555)</label>
+        <input type="text" id="homePhone" placeholder="Please Enter Home Phone #" name="homePhone" value="<?php print $homePhone; ?>" >
 
-        <label for="cellPhone">Cell Phone: (Format: 555-555-5555)</label>
-        <input type="text" id="cellPhone" placeholder="Please Enter Cell Phone #" name="cellPhone" value="<?php print $cellPhone; ?>" required>
+        <label for="cellPhone">Change Cell Phone: (Format: 555-555-5555)</label>
+        <input type="text" id="cellPhone" placeholder="Please Enter Cell Phone #" name="cellPhone" value="<?php print $cellPhone; ?>" >
 
-        <label>Terms and Conditions: <?php print $termsReq; ?></label>
-        <input type="checkbox" id="terms" value="terms" name="terms" required><label class="light" for="terms">I Agree to the Terms and Conditions:</label>
-
-        <button name="enter" class="btn" type="submit">Sign Up</button>
+        <button name="enter" class="btn" type="submit">Submit Changes</button>
     </form>
 
 </div>
